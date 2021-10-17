f4a588c028079b940d665824f63e6bfa66ea3f21
freyja <mrender2005@gmail.com>
2021-10-17 15:30:56 -0700

    newline

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	args := flag.Args()
 
 	if len(args) > 0 { // parse arguments...
-		fmt.Print(parse(strings.Join(args, " "), *key))
+		fmt.Println(parse(strings.Join(args, " "), *key))
 	} else { // parse stdin
 		scanner := bufio.NewScanner(os.Stdin)
 
