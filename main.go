86887fe7f4a977377f3392c989280b775a303df9
freyja <mrender2005@gmail.com>
2021-10-23 20:32:46 -0700

    -n option: choose to print newline

--- main.go
+++ main.go
@@ -9,13 +9,21 @@ import (
 )
 
 func main() {
-	key := flag.Bool("k", false, "Use keyboard substitution mode")
+	var (
+		key     = flag.Bool("k", false, "Use keyboard substitution mode")
+		newline = flag.Bool("n", false, "Print a newline when argumentss are passed")
+	)	
+
 	flag.Parse()
 
 	args := flag.Args()
 
 	if len(args) > 0 { // parse arguments...
-		fmt.Println(parse(strings.Join(args, " "), *key))
+		fmt.Print(parse(strings.Join(args, " "), *key))
+
+		if *newline {
+			fmt.Print("\n")
+		}
 	} else { // parse stdin
 		scanner := bufio.NewScanner(os.Stdin)
 
