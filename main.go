118a7093a3db8672d3ce1a8dfe7dc10440949290
freyja <mrender2005@gmail.com>
2022-07-25 12:47:24 -0700

    add help table

--- main.go
+++ main.go
@@ -8,24 +8,83 @@ import (
 	"strings"
 )
 
+const betaTable string = `Greek Beta Code
+---------------
+    α A
+    β B
+    γ G
+    δ D
+    ε E
+    ϝ V
+    ζ Z
+    η H
+    Θ Q
+    ι I
+    κ K
+    λ L
+    μ M
+    ν N
+    ξ C
+    ο O
+    π P
+    ρ R
+    σ S, S1
+    ς S, S2, J
+    ϲ S3
+    τ T
+    υ U
+    φ F
+    χ X
+    ψ Y
+    ω W
+
+*A -> Α
+
+Puncuation Beta Code
+--------------------
+         . .
+	 , ,
+	 · :
+	 ; ;
+	 ‐ -
+	 — _
+	 ʹ #
+
+        Diacritic BetaCode
+	------------------
+ Smooth Breathing )
+  Rough Breathing (
+    Accute Accent /
+Circumflex Accent =
+     Grave Accent \
+        Diaeresis +
+   Iota Subscript |
+           Macron &
+	    Breve '`
+
 func main() {
 	newline := flag.Bool("n", false, "Don't Print a newline when argumentss are passed")	
+	table := flag.Bool("t", false, "Display a Beta Code usage table")
 
 	flag.Parse()
 
 	args := flag.Args()
 
-	if len(args) > 0 { // parse arguments...
-		fmt.Print(parse(strings.Join(args, " ")))
+	if *table {
+		fmt.Println(betaTable)
+	} else {
+		if len(args) > 0 { // parse arguments...
+			fmt.Print(parse(strings.Join(args, " ")))
 
-		if !*newline {
-			fmt.Print("\n")
-		}
-	} else { // parse stdin
-		scanner := bufio.NewScanner(os.Stdin)
+			if !*newline {
+				fmt.Print("\n")
+			}
+		} else { // parse stdin
+			scanner := bufio.NewScanner(os.Stdin)
 
-		for scanner.Scan() {
-			fmt.Println(parse(scanner.Text()))
+			for scanner.Scan() {
+				fmt.Println(parse(scanner.Text()))
+			}
 		}
 	}
-}
+} 
